045337cf30d2d61e5e1ae9fbd7def8e74c662223
Kaspar Schleiser <kaspar@schleiser.de>
2023-06-06 14:12:46 +0200

    allow selection of profile via cli

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use anyhow::Result;
-use clap::{crate_version, Command};
+use clap::{crate_version, Arg, Command};
 use env_logger::Env;
 use log::debug;
 
@@ -11,30 +11,47 @@ use signal::SignalConfig;
 
 #[tokio::main(flavor = "multi_thread")]
 async fn main() -> Result<()> {
+    fn profile() -> Arg {
+        Arg::new("profile")
+            .help("profile to use")
+            .default_value("default")
+            .env("SIGBOT_PROFILE")
+            .value_parser(clap::value_parser!(String))
+    }
+
     let matches = Command::new("SigBot")
         .version(crate_version!())
         .author("Kaspar Schleiser <kaspar@schleiser.de>")
         .about("A signal bot")
         .infer_subcommands(true)
-        .subcommand(Command::new("run").about("Runs the program"))
-        .subcommand(Command::new("link").about("Create new device link"))
+        .subcommand(Command::new("run").about("Runs the program").arg(profile()))
+        .subcommand(
+            Command::new("link")
+                .about("Create new device link")
+                .arg(profile()),
+        )
         .get_matches();
 
     match matches.subcommand() {
-        Some(("run", matches)) => run_command().await,
-        Some(("link", matches)) => link_command().await,
+        Some(("run", matches)) => {
+            let profile: &String = matches.get_one("profile").unwrap();
+            run_command(profile).await
+        }
+        Some(("link", matches)) => {
+            let profile: &String = matches.get_one("profile").unwrap();
+            link_command(profile).await
+        }
         _ => Err(anyhow::anyhow!("Invalid command")),
     }
 }
 
-async fn run_command() -> anyhow::Result<()> {
+async fn run_command(profile: &str) -> anyhow::Result<()> {
     env_logger::Builder::from_env(
         Env::default().default_filter_or(format!("{}=warn", env!("CARGO_PKG_NAME"))),
     )
     .init();
 
-    let signal = SignalConfig::new("tester")?
-        //.register_handler(Box::new(debug::DebugHandler {}))
+    let signal = SignalConfig::new(profile)?
         .register_handler(Box::new(bot::Bot::new()))
         .run(signal::timestamp())?;
 
@@ -43,6 +60,6 @@ async fn run_command() -> anyhow::Result<()> {
     Ok(())
 }
 
-async fn link_command() -> anyhow::Result<()> {
-    SignalConfig::new("tester")?.link().await
+async fn link_command(profile: &str) -> anyhow::Result<()> {
+    SignalConfig::new(profile)?.link().await
 }
